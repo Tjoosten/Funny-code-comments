277d69070fad72304036feb4a391a22e83286ba4
Tim Joosten <Tjoosten@users.noreply.github.com>
2015-01-02 02:51:19 +0100

    Create Dear Maintainer.php

---
+++ Dear Maintainer.php
@@ -0,0 +1,10 @@
+// 
+// Dear maintainer:
+// 
+// Once you are done trying to 'optimize' this routine,
+// and have realized what a terrible mistake that was,
+// please increment the following counter as a warning
+// to the next guy:
+// 
+// total_hours_wasted_here = 42
+// 
